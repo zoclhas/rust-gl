c7789d49e182a808f28096d385ff837b088533e6
zoclhas <zoclhas@gmail.com>
2024-02-09 18:41:10 +0400

    Blue coloured window

--- src/bin/section_04.rs
+++ src/bin/section_04.rs
@@ -32,7 +32,7 @@ fn main() {
         process_events(&mut window, &events);
 
         unsafe {
-            gl::ClearColor(0.1, 0.2, 0.3, 1.0);
+            gl::ClearColor(0.3, 0.2, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
             window.swap_buffers();
@@ -40,5 +40,4 @@ fn main() {
         }
     }
 }
-
 
