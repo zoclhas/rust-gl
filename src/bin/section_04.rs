b5234baca2624df2525b94253ce234d5d1f0bf23
zoclhas <zoclhas@gmail.com>
2024-02-09 18:19:40 +0400

    Create window

---
+++ src/bin/section_04.rs
@@ -0,0 +1,18 @@
+use glfw::Context;
+
+extern crate glfw;
+
+fn main() {
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+
+    let (mut window, _events) = glfw
+        .create_window(800, 600, "Hello!", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+    }
+}
