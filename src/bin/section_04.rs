515fee990c79ce05ba614b228ecf4b365c218b11
zoclhas <zoclhas@gmail.com>
2024-02-09 18:41:02 +0400

    Blue coloured window

--- src/bin/section_04.rs
+++ src/bin/section_04.rs
@@ -2,18 +2,43 @@ use glfw::Context;
 
 extern crate glfw;
 
+fn process_events(
+    window: &mut glfw::Window,
+    events: &glfw::GlfwReceiver<(f64, glfw::WindowEvent)>,
+) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
-    let (mut window, _events) = glfw
+    let (mut window, events) = glfw
         .create_window(800, 600, "Hello!", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     window.set_key_polling(true);
     window.make_current();
-    window.swap_buffers();
+
+    gl::load_with(|sym| window.get_proc_address(sym) as *const _);
 
     while !window.should_close() {
-        glfw.poll_events();
+        process_events(&mut window, &events);
+
+        unsafe {
+            gl::ClearColor(0.1, 0.2, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            window.swap_buffers();
+            glfw.poll_events();
+        }
     }
 }
+
+
